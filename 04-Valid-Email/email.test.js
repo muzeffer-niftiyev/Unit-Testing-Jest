ef9cb66400045c3d2b8a28c628fe5133eacbe316
Muzeffer <muzeffer9b2004@gmail.com>
2023-09-06 19:04:35 +0400

    Email Validation test complate

--- 04-Valid-Email/email.test.js
+++ 04-Valid-Email/email.test.js
@@ -1,7 +1,7 @@
 const { validateEmail } = require("./email");
 
 describe("----Valid-Email----", () => {
-  describe("Validate Email function: ", () => {
+  describe("validateEmail function: ", () => {
     let email;
 
     beforeEach(() => {
@@ -16,39 +16,39 @@ describe("----Valid-Email----", () => {
       expect(validateEmail(email)).toBeTruthy();
 
       expect(validateEmail("email.com")).toBeFalsy();
-      expect(validateEmail('email.az')).toBeFalsy();
+      expect(validateEmail("email.az")).toBeFalsy();
       expect(validateEmail("email.com.@")).toBeFalsy();
     });
 
     test('should check if email includes "@" ', () => {
-        expect(validateEmail('email@mail.com')).toBeTruthy();
-        expect(validateEmail('email.mail.com')).toBeFalsy();
-    })
+      expect(validateEmail("email@mail.com")).toBeTruthy();
+      expect(validateEmail("email.mail.com")).toBeFalsy();
+    });
 
     test("returned string shouldn't start with '@' ", () => {
-        expect(validateEmail('@.email.com')).toBeFalsy();
-        expect(validateEmail('@.com')).toBeFalsy();
-        expect(validateEmail('@')).toBeFalsy();
-    })
+      expect(validateEmail("@.email.com")).toBeFalsy();
+      expect(validateEmail("@.com")).toBeFalsy();
+      expect(validateEmail("@")).toBeFalsy();
+    });
 
     test('should check if email includes ".com" ', () => {
-        expect(validateEmail('email@.com')).toBeTruthy();
-        expect(validateEmail('me@.ru')).toBeFalsy();
-        expect(validateEmail('email@gmail.az')).toBeFalsy();
-    })
+      expect(validateEmail("email@.com")).toBeTruthy();
+      expect(validateEmail("me@.ru")).toBeFalsy();
+      expect(validateEmail("email@gmail.az")).toBeFalsy();
+    });
 
     test('returned string should end with ".com" ', () => {
-        expect(validateEmail('mail@.gmail.com')).toBeTruthy();
-        expect(validateEmail('.com.@.email')).toBeFalsy();
-        expect(validateEmail('gmail.com.@')).toBeFalsy();
-        expect(validateEmail('gamil@.mailcom')).toBeFalsy();
-    })
+      expect(validateEmail("mail@.gmail.com")).toBeTruthy();
+      expect(validateEmail(".com.@.email")).toBeFalsy();
+      expect(validateEmail("gmail.com.@")).toBeFalsy();
+      expect(validateEmail("gamil@.mailcom")).toBeFalsy();
+    });
 
-    test('should be called with only strings', () => {
-        expect(() => validateEmail(100)).toThrow();
-        expect(() => validateEmail([email, mail])).toThrow();
-        expect(() => validateEmail({})).toThrow();
-        expect(() => validateEmail(null)).toThrow();
-    })
+    test("should be called with only strings", () => {
+      expect(() => validateEmail(100)).toThrow();
+      expect(() => validateEmail([email, mail])).toThrow();
+      expect(() => validateEmail({})).toThrow();
+      expect(() => validateEmail(null)).toThrow();
+    });
   });
 });
